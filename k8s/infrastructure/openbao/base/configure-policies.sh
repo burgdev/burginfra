b4ce47ddb447543ee931f0734792262b1bda506d
Tobias Burgherr <tburgherr@gmail.com>
2025-11-16 01:24:42 +0100

    sleep for 10min

--- k8s/infrastructure/openbao/base/configure-policies.sh
+++ k8s/infrastructure/openbao/base/configure-policies.sh
@@ -18,8 +18,8 @@ echo "SA_TOKEN: $SA_TOKEN"
 if ! BAO_TOKEN=$(bao write -field=token auth/kubernetes/login role=openbao-admin jwt="$SA_TOKEN" 2>&1); then
 	echo "ERROR: Failed to authenticate to OpenBao"
 	echo "$BAO_TOKEN"
-	echo "Sleep for 5min"
-	sleep 300
+	echo "Sleep for 10min"
+	sleep 600
 	exit 1
 fi
 
